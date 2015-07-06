19866505ad8f6b833d0a81a1e78c114e3d561ec2
Guillaume Lagorce <gul@octo.com>
2015-07-06 15:49:39 +0200

    Set up bot

---
+++ YourSeries/Classes/Controller/YSEPageViewController.h
@@ -0,0 +1,13 @@
+//
+//  YSEPageViewController.h
+//  YourSeries
+//
+//  Created by Guillaume Lagorce on 15/06/2015.
+//  Copyright (c) 2015 octo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YSEPageViewController : UIPageViewController
+
+@end
---
+++ YourSeries/Classes/Controller/YSEPageViewController.m
@@ -0,0 +1,28 @@
+//
+//  YSEPageViewController.m
+//  YourSeries
+//
+//  Created by Guillaume Lagorce on 15/06/2015.
+//  Copyright (c) 2015 octo. All rights reserved.
+//
+
+#import "YSEPageViewController.h"
+#import "YSEMovieDetailsViewController.m"
+
+@interface YSEPageViewController () <UIPageViewControllerDataSource>
+
+@end
+
+@implementation YSEPageViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
